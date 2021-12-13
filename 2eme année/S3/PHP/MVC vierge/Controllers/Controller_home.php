9068ed8038e3bf92944f588f89c33e4c542c7346
RIIDLEY <hamsek.lahoucine@gmail.com>
2021-12-13 10:52:47 +0100

    mvc model creat

--- 2eme année/S3/PHP/MVC vierge/Controllers/Controller_home.php
+++ 2eme année/S3/PHP/MVC vierge/Controllers/Controller_home.php
@@ -4,9 +4,7 @@ class Controller_home extends Controller{
 
   public function action_home(){
     $m = Model::getModel();
-    $tab = ['nb_nobels' => $m->getNbNobelPrizes()];
-
-    $this->render('home', $tab);
+    $this->render('home');
   }
 
   public function action_default(){
